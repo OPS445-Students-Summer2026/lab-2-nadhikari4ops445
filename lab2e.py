406cfd00c6cdf1071c640f647ba0cfdc4e61ddb2
Nidhish Adhikari <nadhikari4@myseneca.ca>
2026-06-04 16:34:46 -0400

    completed lab2

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Nidhish Adhikari
+# Author ID: nadhikari4
+# Date Created: 2026/06/04
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Nidhish Adhikari
+# Author ID: nadhikari4
+# Date Created: 2026/06/04
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
